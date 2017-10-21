adad1395b203a29018bdb2984d4c116c49b77422
Ron Danon <rondnaon@gmail.com>
2017-10-21 14:43:10 +0300

    change Init method signature to avoid reported bugs

--- src/PID.cpp
+++ src/PID.cpp
@@ -21,11 +21,11 @@ PID::PID() {
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_, double Ki_, double Kd_) {
 	//set the PID hyper parameters
-	this->Kp = Kp;
-	this->Ki = Ki;
-	this->Kd = Kd;
+	Kp = Kp_;
+	Ki = Ki_;
+	Kd = Kd_;
 	//initialize error values
 	p_error = 0;
 	i_error = 0;
