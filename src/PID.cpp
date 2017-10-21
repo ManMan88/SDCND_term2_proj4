cb29fdd5e94b9df47d207ac4a44855d947c8df48
Ron Danon <rondnaon@gmail.com>
2017-10-21 12:36:33 +0300

    complete PID constructior and Init methods

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,11 +6,32 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+	//initialize error values
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
+	//initialize PID hyper parameters values
+	Kp = 0;
+	Ki = 0;
+	Kd = 0;
+
+	return;
+}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	//set the PID hyper parameters
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+	//initialize error values
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
+
+	return;
 }
 
 void PID::UpdateError(double cte) {
@@ -18,4 +39,5 @@ void PID::UpdateError(double cte) {
 
 double PID::TotalError() {
 }
+
 
